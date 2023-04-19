fea60f6df2f292af8686492ab5e6fa97d805de0e
Mariana Mena <maranashokry@ahoo.com>
2023-04-19 17:58:50 -0500

    added (Book , Genre) Models , Database and starter Data for AgeRange, Genre, CoverType and Book tables.

---
+++ ReadersRendezvous/Models/Books.cs
@@ -0,0 +1,20 @@
+﻿using ReadersRendezvous.Models;
+
+namespace ReadersRendezvous.Models
+{
+    public class Books
+    {
+        public int Id { get; set; }
+        public int GenreId { get; set; }
+        public string Title { get; set; }
+        public int CoverTypeId { get; set; }
+        public int Quantity { get; set; } = 1;
+        public string Author { get; set; }
+        public string Publisher { get; set; }
+        public string Language { get; set; } = "English";
+        public string Description { get; set; }
+        public string ISBN13 { get; set; }
+
+    }
+}
+
