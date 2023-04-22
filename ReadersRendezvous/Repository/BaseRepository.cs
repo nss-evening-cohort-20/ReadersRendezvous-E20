8c8324f4c97c9e2d21195490a860e31cee7c0cea
John Behman <behmanjohn@gmail.com>
2023-04-22 11:49:46 -0500

    john commit

--- ReadersRendezvous/Repository/BaseRepository.cs
+++ ReadersRendezvous/Repository/BaseRepository.cs
@@ -3,8 +3,8 @@
 using Microsoft.Data.SqlClient;
     public abstract class BaseRepository
     {
-        {
+       
     }
     private string _connectionString;
     protected SqlConnection Connection => new SqlConnection(_connectionString);
-}
+
