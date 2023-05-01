f8f93349678ac3b7cff3a9d0c13ef4b6ea001d7a
Demin Zawity <106280855+DeminZawity@users.noreply.github.com>
2023-05-01 18:29:36 -0500

    Fixed some errors with the User Repository for the insert and update methods

--- ReadersRendezvous/Controllers/UserBookController.cs
+++ ReadersRendezvous/Controllers/UserBookController.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using ReadersRendezvous.Interfaces;
 using ReadersRendezvous.Models;
 using ReadersRendezvous.Repository;
 
 namespace ReadersRendezvous.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class UserBookController : ControllerBase
