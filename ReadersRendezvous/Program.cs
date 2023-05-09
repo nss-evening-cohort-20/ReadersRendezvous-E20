669c5f04557ad0355d962e43a63e0128d8267b7d
John Behman <behmanjohn@gmail.com>
2023-05-08 19:08:18 -0500

    fixing error 2

--- ReadersRendezvous/Program.cs
+++ ReadersRendezvous/Program.cs
@@ -63,24 +63,7 @@ namespace ReadersRendezvous
                 });
             });
 
-            FirebaseApp.Create(new AppOptions()
-            {
-                Credential = GoogleCredential.FromFile(builder.Configuration["FirebaseConfig"]),
-            });
-
-            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
-            {
-                options.IncludeErrorDetails = true;
-                options.Authority = "https://securetoken.google.com/readersrendezvous-e20-80a40"; //use your project name
-                options.TokenValidationParameters = new TokenValidationParameters
-                {
-                    ValidateIssuer = true,
-                    ValidIssuer = "https://securetoken.google.com/readersrendezvous-e20-80a40", //use your project name
-                    ValidateAudience = true,
-                    ValidAudience = "readersrendezvous-e20-80a40",  //use your project name
-                    ValidateLifetime = true,
-                };
-            });
+       
 
             //builder.Services.AddAuthentication(NegotiateDefaults.AuthenticationScheme)
             //    .AddNegotiate();
