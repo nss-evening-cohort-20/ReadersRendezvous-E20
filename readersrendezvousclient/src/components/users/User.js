0da2590e42be6982631f8dca6cc993c54c785af0
John Behman <behmanjohn@gmail.com>
2023-05-08 19:07:56 -0500

    fixing  error

--- readersrendezvousclient/src/components/users/User.js
+++ readersrendezvousclient/src/components/users/User.js
@@ -1,8 +1,48 @@
 import React from "react";
-export const User = () => { 
-    return (
-        <>
-            <h1>All Users</h1>
-        </>
-)
-}
+import { Link } from "react-router-dom"
+import { UserDetails } from "./UserDetails";
+import "./Users.css"
+
+
+
+
+
+export const User = ({ userObj }) => {
+
+
+    return <div>
+        {/* <section className="user">
+
+
+            <div>
+                <Link to={`/users/${id}`}>Name: {firstName}</Link>
+            </div>
+
+        </section> */}
+        <section className="userTest">
+            <div>
+                <Link to={`/users/${userObj.id}`}>Name:{`${userObj.firstName} ${userObj.lastName}`}</Link>
+            </div>
+            <div className="users_8">
+                <div>Email: {userObj.email}</div>
+                <div>LibraryCardNumber: {userObj.libraryCardNumber}</div>
+                <div>IsActive: {userObj.isActive}</div>
+                <div>PhoneNumber: {userObj.phoneNumber}</div>
+                <div>AddressLineOne: {userObj.addressLineOne}</div>
+                <div>AddressLineTwo: {userObj.addressLineTwo}</div>
+                <div>City: {userObj.city}</div>
+                <div>State: {userObj.state}</div>
+                <div>Zip: {userObj.zip}</div>
+            </div>
+        </section>
+
+
+
+    </div >
+
+};
+
+
+
+
+
