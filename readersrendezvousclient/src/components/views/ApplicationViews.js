a101e64788e9f83e79ecf02dcfab3878eee30656
taylo <taylor00201@gmail.com>
2023-05-09 18:29:11 -0500

    updates app for logout functionality

--- readersrendezvousclient/src/components/views/ApplicationViews.js
+++ readersrendezvousclient/src/components/views/ApplicationViews.js
@@ -11,37 +11,28 @@ import { AdminProfile } from "../Admin/AdminProfile";
 import { AddAdmin } from "../Admin/AddAdmin";
 import { EditAdmin } from "../Admin/EditAdmin";
 import { Login } from "../auth/Login";
-
+import { Logout } from "../auth/Logout";
 
 export const ApplicationViews = () => {
-    return (
-        <React.StrictMode>
-            <Routes>
-                <Route path="home" element={<Home />} />
-                <Route path="books" element={<BookList />} />
-                <Route path="users" element={<UserList />} />
-                <Route path="books/:bookId" element={<BookDetails />} />
-                {/* <Route path="users/:userId" element={<Userlist />} /> */}
-                {/* <Route path="books/:bookId" element={<BookDetails />} /> */}
-                <Route path="books/:bookId" element={<BookDetails />} />
-                <Route path="books/edit/:bookEditId" element={<EditBook />} />  
-                <Route path="editBook" element={<EditBook />} />
-                <Route path="users" element={<User />} />
-                <Route path="addBook" element={<AddBook />} />
-                <Route path="adminProfile" element={<AdminProfile />} />
-                <Route path="addAdmin" element={<AddAdmin />} />
-                <Route path="editAdmin" element={<EditAdmin />} />
-            </Routes>
-        </React.StrictMode>
-    );
   return (
     <React.StrictMode>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="login" element={<Login />} />
-        <Route path="books" element={<BookList />} />
-        <Route path="books/:bookId" element={<BookDetails />} />
-        <Route path="users" element={<User />} />
+        <Route path='home' element={<Home />} />
+        <Route path='books' element={<BookList />} />
+        <Route path='users' element={<UserList />} />
+        <Route path='books/:bookId' element={<BookDetails />} />
+        {/* <Route path="users/:userId" element={<Userlist />} /> */}
+        {/* <Route path="books/:bookId" element={<BookDetails />} /> */}
+        <Route path='books/:bookId' element={<BookDetails />} />
+        <Route path='books/edit/:bookEditId' element={<EditBook />} />
+        <Route path='editBook' element={<EditBook />} />
+        <Route path='users' element={<User />} />
+        <Route path='addBook' element={<AddBook />} />
+        <Route path='adminProfile' element={<AdminProfile />} />
+        <Route path='login' element={<Login />} />
+        <Route path='addAdmin' element={<AddAdmin />} />
+        <Route path='editAdmin' element={<EditAdmin />} />
+        <Route path='Login' element={<Logout />} />
       </Routes>
     </React.StrictMode>
   );
