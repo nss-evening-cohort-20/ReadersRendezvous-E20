f641533b8f2974cad0567de4c0b7164eeeb53f79
Demin Zawity <106280855+DeminZawity@users.noreply.github.com>
2023-05-13 04:17:45 -0500

    Styling has been completed for the login page and the admin pages.

--- readersrendezvousclient/src/components/Admin/AdminProfile.js
+++ readersrendezvousclient/src/components/Admin/AdminProfile.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import React from "react";
 import "./AdminProfileStyle.css";
-
+import {NameCircle} from "./NameCircle"
 
 export const AdminProfile = () => {
 const [adminsProfile, setAdminProfile] = useState([])
@@ -25,18 +25,44 @@ useEffect(() => {
 
 
   return (
-    <div className = "AdminProfilePageContainer">
-        <div className="AdminProfileContainer">
+    // <div className = "AdminProfilePageContainer">
+    //     <div className="AdminProfileContainer">
 
-                <section className="adminProfileSection">
-                    <div className="EditAdminButton" onClick={() => navigate(`/editAdmin/${adminsProfile.id}`)}>Edit Admin</div>
-                    <div className="adminProfileDetail">Name: {adminsProfile.firstName} {adminsProfile.lastName}</div>
-                    <div className="adminProfileDetail">Email: {adminsProfile.email}</div>
-                </section>
+    //             <section className="adminProfileSection">
+    //                 <div className="EditAdminButton" onClick={() => navigate(`/editAdmin/${adminsProfile.id}`)}>Edit Admin</div>
+    //                 <div className="adminProfileDetail">Name: {adminsProfile.firstName} {adminsProfile.lastName}</div>
+    //                 <div className="adminProfileDetail">Email: {adminsProfile.email}</div>
+    //             </section>
 
-        <div className="AddAdminButton" onClick={() => navigate("/addAdmin")}>Add Admin</div>
+    //     <div className="AddAdminButton" onClick={() => navigate("/addAdmin")}>Add Admin</div>
+    //     </div>
+    // </div>
+
+    <div className="MainContainer">
+      <div className="CenterContainer">
+        <div className="Card">
+          <div className="header">Library Card</div>
+          <div className="img">
+            <NameCircle />
+          </div>
+          <div className='footer'>
+            <div className="Name">
+            {adminsProfile.firstName} {adminsProfile.lastName}
+            </div>
+            <div className="Email">
+            {adminsProfile.email}
+            </div>
+          </div>
         </div>
+        <div className="EditButton" onClick={() => navigate(`/editAdmin/${adminsProfile.id}`)}>
+          Edit
+        </div>
+        <div className="addAdminButton" onClick={() => navigate("/addAdmin")}>
+        Add New
+      </div>
+      </div>
     </div>
+
       );
 
 
